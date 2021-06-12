8ace492ecf97c200d9f6a97499f07c622c402ef3
Mateus <teuspersi@gmail.com>
2021-06-11 21:38:09 -0300

    Adiciona autenticação para manipular CRUD

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,4 +1,5 @@
 class HerosController < ApplicationController
+  http_basic_authenticate_with name: Rails.application.credentials.authenticate[:name], password: Rails.application.credentials.authenticate[:password], except: [:index, :show]
   before_action :set_hero, only: %i[ show edit update destroy ]
 
   # GET /heros or /heros.json
